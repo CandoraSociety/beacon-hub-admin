55cf0c9f4c66b4c3a9f801a440fe2ba2a40e8b2b
graham.currie <graham.currie@candorasociety.com>
2026-05-16 00:12:54 +0000

    File changes

--- src/hooks/useBranding.js
+++ src/hooks/useBranding.js
@@ -18,7 +18,7 @@ function hexToHsl(hex) {
 export function useBranding() {
   useEffect(() => {
     const apply = () => {
-      fetch('https://beacon-92324875.base44.app/functions/getBranding')
+      fetch('https://beacon-nexus-core.base44.app/functions/getBranding')
         .then(r => r.json())
         .then(({ primary_color, secondary_color, background_color }) => {
           if (primary_color) {
